dabdc1e9f41e465c1b9f36b265bd8f51e8ff4a5c
Joseph Jones <jjones15470@hotmail.com>
2015-12-18 16:00:03 -0700

    Added SDL and SDL_image libraries and a test

--- Critter-Keeper/Critter-Keeper/App.cpp
+++ Critter-Keeper/Critter-Keeper/App.cpp
@@ -3,13 +3,92 @@
 
 using namespace std;
 
-int main() {
+App::App(std::string text) {
+	message = text;
+	running = true;
+	SDL_Init(SDL_INIT_EVERYTHING);
+	IMG_Init(IMG_INIT_PNG);
+	cout << "Initialized SDL" << endl;
+}
+
+void App::init() {
+	// Creating the window object
+	window = SDL_CreateWindow("Critter Keeper", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_SHOWN);
+
+	// Creating a surface to draw on from the window
+	surf = SDL_GetWindowSurface(window);
+
+	// Creating a black background
+	SDL_FillRect(surf, NULL, SDL_MapRGB(surf->format, 200, 200, 200));
+
+	// Update the surface
+	SDL_UpdateWindowSurface(window);
+
+	cout << "Initialized window and window surface" << endl;
+}
+
+void App::loadAll() {
+	hello = loadImage("res\\hello.png");
+
+	cout << "Loaded all required images" << endl;
+}
+
+void App::update() {
+	while (running) {
+		while (SDL_PollEvent(&ev) != 0) {
+			if (ev.type == SDL_QUIT) {
+				running = false;
+			}
+		}
+
+		// Draw all surfaces
+		SDL_BlitSurface(hello, NULL, surf, NULL);
+
+		// Update the window surface
+		SDL_UpdateWindowSurface(window);
+	}
+}
+
+SDL_Surface* App::loadImage(std::string path) {
+	SDL_Surface* newSurf = NULL;
+
+	SDL_Surface* loaded = IMG_Load(path.c_str());
+	if (loaded == NULL) {
+		cerr << "Unable to load image " << path.c_str() << "! SDL_Image Error: " << IMG_GetError() << endl;
+	}
+	else {
+		newSurf = SDL_ConvertSurface(loaded, surf->format, NULL);
+		SDL_FreeSurface(loaded);
+	}
+
+	cout << "Loaded image: " << path << endl;
+
+	return newSurf;
+}
+
+void App::exit() {
+	SDL_DestroyWindow(window);
+	SDL_FreeSurface(surf);
+	SDL_FreeSurface(hello);
+
+	SDL_Quit();
+	
+	cout << "Freeing surfaces and window\nExiting SDL" << endl;
+}
+
+int main(int argc, char* argv[]) {
 	App myApp = App("Hello");
+	myApp.init();
+	myApp.loadAll();
+
+	myApp.update();
 
 	cout << myApp.getMessage() << endl;
 
-	cin.get();
-	cin.ignore();
+	//cin.get();
+	//cin.ignore();
+
+	myApp.exit();
 
 	return 0;
 }
--- Critter-Keeper/Critter-Keeper/App.h
+++ Critter-Keeper/Critter-Keeper/App.h
@@ -1,16 +1,47 @@
 #pragma once
 #include <string>
+#include <SDL.h>
+#include <SDL_image.h>
 
 class App {
 	private:
 		std::string message;
+		bool running;
+		SDL_Event ev;
+		SDL_Window* window = NULL;
+		SDL_Renderer* rend = NULL;
+		SDL_Surface* surf = NULL;
+		SDL_Surface* hello = NULL;
 
 	public:
-		App(std::string text) {
-			message = text;
-		}
+		const int WIDTH = 1280;
+		const int HEIGHT = 720;
+
+		App(std::string text);
+
+		void init();
+
+		void loadAll();
+
+		void update();
+
+		SDL_Surface* loadImage(std::string path);
+
+		void exit();
 
 		std::string getMessage() {
 			return message;
+		}
+
+		SDL_Window* getWindow() {
+			return window;
+		}
+
+		SDL_Surface* getSurf() {
+			return surf;
+		}
+
+		SDL_Surface* getHello() {
+			return hello;
 		}
 };
